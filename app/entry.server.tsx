55bd252a456dfa8a657b12f738b75ff4010d9d43
IntanNanda <intan.irnanda@sera.astra.co.id>
2025-08-19 22:29:19 +0700

    ver:0.0.1 tkt:none msg:update

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,6 +1,8 @@
 import { createRequestHandler } from "@react-router/express"
 import type { ServerBuild } from "react-router"
 
+
+
 export default function createServer(build: ServerBuild) {
 	return createRequestHandler({ build })
 }
